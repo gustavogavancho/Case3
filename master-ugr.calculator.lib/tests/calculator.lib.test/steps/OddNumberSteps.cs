c4b627bcba736d7bb72453cfc7e330bb7e33748e
Gustavo Gavancho León <gustavo.gavancho.l@live.com>
2025-01-17 09:20:42 -0500

    Divide by 0 feature

--- master-ugr.calculator.lib/tests/calculator.lib.test/steps/OddNumberSteps.cs
+++ master-ugr.calculator.lib/tests/calculator.lib.test/steps/OddNumberSteps.cs
@@ -1,40 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TechTalk.SpecFlow;
 
-namespace calculator.lib.test.steps
+namespace calculator.lib.test.steps;
+
+[Binding]
+public class OddNumberSteps
 {
-    [Binding]
-    public class OddNumberSteps
+    private readonly ScenarioContext _scenarioContext;
+    public OddNumberSteps(ScenarioContext scenarioContext)
     {
-        private readonly ScenarioContext _scenarioContext;
-        public OddNumberSteps(ScenarioContext scenarioContext)
-        {
-            _scenarioContext = scenarioContext;
-        }
+        _scenarioContext = scenarioContext;
+    }
 
-        [Given(@"a number (.*)")]
-        public void WhenNumberIsChecked(int number)
-        {
-            _scenarioContext.Add("number", number);
-        }
+    [Given(@"a number (.*)")]
+    public void WhenNumberIsChecked(int number)
+    {
+        _scenarioContext.Add("number", number);
+    }
 
-        [When("I check if it is odd")]
-        public void ICheckIfItIsOdd()
-        {
-            var number = _scenarioContext.Get<int>("number");
-            var isOdd = NumberAttributter.IsOdd(number);
-            _scenarioContext.Add("isOdd", isOdd);
-        }
+    [When("I check if it is odd")]
+    public void ICheckIfItIsOdd()
+    {
+        var number = _scenarioContext.Get<int>("number");
+        var isOdd = NumberAttributter.IsOdd(number);
+        _scenarioContext.Add("isOdd", isOdd);
+    }
 
-        [Then("it should be odd (.*)")]
-        public void ItShouldBeOdd(bool expected)
-        {
-            var isOdd = _scenarioContext.Get<bool>("isOdd");
-            Assert.Equal(expected, isOdd);
-        }
+    [Then("it should be odd (.*)")]
+    public void ItShouldBeOdd(bool expected)
+    {
+        var isOdd = _scenarioContext.Get<bool>("isOdd");
+        Assert.Equal(expected, isOdd);
     }
 }
